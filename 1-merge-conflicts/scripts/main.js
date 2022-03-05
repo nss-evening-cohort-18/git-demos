2f0a49d689afd45f76be707fe7fb009207ba01cc
David Yarbrough <yarbroughdavidc@gmail.com>
2022-03-05 08:42:28 -0600

    renaming database to data. for reasons

--- 1-merge-conflicts/scripts/main.js
+++ 1-merge-conflicts/scripts/main.js
@@ -1,12 +1,12 @@
-import { database } from "./database.js";
+import { data } from "./database.js";
 
-console.log(`the database is: \n ${JSON.stringify(database)}`)
+console.log(`the database is: \n ${JSON.stringify(data)}`)
 
 // This function takes in a hotel id and will iterate through the rooms
 // and take a total count of rooms have the same hotel_id as the argument,
 // and return the sum
 const getNumberOfRooms = (hotel_id) => {
-    const rooms = database.rooms
+    const rooms = data.rooms
 
     let roomCount = 0
     for (const room of rooms) {
@@ -17,4 +17,22 @@ const getNumberOfRooms = (hotel_id) => {
     return roomCount
 }
 
-console.log(getNumberOfRooms(1))
+const getHotelAddress = (hotel_id) => {
+    let hotelAddress = null;
+
+    for (hotel of data.hotels) {
+        if (hotel_id === hotel.id) {
+            address = hotel.address
+            for (address of data.addresses) {
+                if (hotelAddress === address.id ) {
+                    hotelAddress = address
+                }
+            }
+        }
+    }
+
+    if (hotelAddress === null) {
+        console.warn(`no address found for hotel ID: ${hotel_id}`)
+    }
+    return hotelAddress
+}
